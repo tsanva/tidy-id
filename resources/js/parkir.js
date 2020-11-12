b68ed3a742386af36acf68f0c27c288cac5c9e58
tsanva <vasantpns@gmail.com>
2020-11-13 02:30:35 +0700

    experimenting new page format for later

---
+++ resources/js/parkir.js
@@ -0,0 +1,8 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var element = document.getElementById("preload");
+    element.classList.remove("no-transition");
+  },
+  false
+);
